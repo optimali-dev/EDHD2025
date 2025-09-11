3586eccb5c2f3530412c7e093af74d84ac2bc64e
Aleks4920 <sash4920@gmail.com>
2025-09-11 23:38:16 +0200

    fix data merge

--- Data/combine.py
+++ Data/combine.py
@@ -1,3 +1,39 @@
+import pandas as pd
+
+# Load both datasets
+sunrise_path = 'Data/fulldata_with_sunrise.csv'
+main_path = 'Data/fulldata_2024_202506.csv'
+
+df_sunrise = pd.read_csv(sunrise_path)
+df_main = pd.read_csv(main_path, delimiter=';')
+
+# Parse datetime columns
+# For sunrise: 'time', for main: 'UTC' or 'Datum / Uhrzeit'
+df_sunrise['time'] = pd.to_datetime(df_sunrise['time'])
+if 'UTC' in df_main.columns:
+	df_main['UTC'] = pd.to_datetime(df_main['UTC'])
+	main_time_col = 'UTC'
+elif 'Datum / Uhrzeit' in df_main.columns:
+	df_main['Datum / Uhrzeit'] = pd.to_datetime(df_main['Datum / Uhrzeit'], dayfirst=True)
+	main_time_col = 'Datum / Uhrzeit'
+else:
+	raise ValueError('No UTC or Datum / Uhrzeit column found in main data!')
+
+# Merge on nearest time (tolerance 1 minute, can be adjusted)
+df_sunrise = df_sunrise.sort_values('time')
+df_main = df_main.sort_values(main_time_col)
+
+# Use merge_asof for nearest match
+combined = pd.merge_asof(
+	df_main, df_sunrise,
+	left_on=main_time_col, right_on='time',
+	direction='nearest',
+	tolerance=pd.Timedelta('1min')
+)
+
+# Save result
+combined.to_csv('Data/fulldata_combined.csv', index=False, sep=';')
+print('Combined file saved to Data/fulldata_combined.csv')
 # combine full C:\Code\Data_cleanUp_step1\Data\fulldata_2024_202506.csv and C:\Code\Data_cleanUp_step1\Data\fulldata_with_sunrise.csv
 import pandas as pd
 import os
